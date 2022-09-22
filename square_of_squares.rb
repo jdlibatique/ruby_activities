b8850e964a442e6b4e1284cb96f0a4a62be3e90c
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-09-22 19:22:15 +0800

    Moved puts around

--- square_of_squares.rb
+++ square_of_squares.rb
@@ -2,21 +2,20 @@ print("Enter a value to evaluate: ")
 user_input = gets.to_i
 
 def check_square (number)
-  if number <= 0
+  if number < 1
+    puts("#{number} is not a perfect square...")
     return false
   end
   counter = 1
   while counter * counter <= number
     if (number % counter == 0) && (number / counter == counter)
+      puts("#{number} is a perfect square!")
       return true
     end
     counter += 1
   end
+  puts("#{number} is not a perfect square...")
   false
 end
 
-if check_square(user_input)
-  puts("Value is a perfect square!")
-else
-  puts("Value is not a perfect square...")
-end
+check_square(user_input)
