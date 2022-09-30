6466f0ea025fe67ae4a99321adf415a2435c2ca9
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-09-30 20:50:37 +0800

    Added isogram file

--- isogram.rb
+++ isogram.rb
@@ -1,9 +1,11 @@
-print "Enter a string to check if it's an isogram: "
-isogram = gets.chomp
+def is_isogram()
 
-def is_isogram(isogram)
+  print "Enter a string to check if it's an isogram: "
+  isogram = gets.chomp
+
   string_array = isogram.downcase.split("").sort
   counter = 0
+
   while counter < string_array.length
     if string_array[counter] == string_array[counter + 1]
       return false
@@ -13,4 +15,4 @@ def is_isogram(isogram)
   true
 end
 
-puts is_isogram(isogram)
+puts is_isogram()
