f14838125b8aa5730464322e0da0a2febdb39576
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-09-30 20:48:28 +0800

    Added isogram file

--- isogram.rb
+++ isogram.rb
@@ -3,7 +3,6 @@ isogram = gets.chomp
 
 def is_isogram(isogram)
   string_array = isogram.downcase.split("").sort
-  puts "#{string_array}"
   counter = 0
   while counter < string_array.length
     if string_array[counter] == string_array[counter + 1]
