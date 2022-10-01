8b52c594ec81a5afed83d6220ae7abdf38839aee
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-01 11:57:31 +0800

    Added db_joins.sql, noticed that db_exercise_1.sql is blank and have no idea what to do

--- SQL/db_joins.sql
+++ SQL/db_joins.sql
@@ -9,11 +9,25 @@
 -- INSERT INTO notes (body, student_id)
 --     VALUES ('Hello World!', 1);
 -- INSERT INTO notes (body, student_id)
---     VALUES ('I wonder if the moon is made of cheese...', NULL);
+--     VALUES ('I wonder if the moon is made of cheese...', null);
 -- INSERT INTO notes (body, student_id)
 --     VALUES ('There''s something I want to say...', 2);
+-- INSERT INTO notes (body, student_id)
+--     VALUES ('I hate cheese!', 2);
+-- INSERT INTO notes (body, student_id)
+-- VALUES ('I need help with my math homework.', 3);
+-- INSERT INTO notes (body, student_id)
+-- VALUES ('There''s something in the water', 4);
+-- INSERT INTO notes (body, student_id)
+-- VALUES ('I think it might be a rat swimming.', 4);
+-- INSERT INTO notes (body, student_id)
+-- VALUES ('Wait, a rat?', 5);
+-- INSERT INTO notes (body, student_id)
+-- VALUES ('I saw it too, it''s a rat!', 6);
+-- INSERT INTO notes (body, student_id)
+-- VALUES ('DFA and NDFA confuses me', null);
 
--- SELECT * FROM notes;
+SELECT * FROM notes;
 --
 -- SELECT * FROM students
 --     INNER JOIN notes n on students.id = n.student_id;
@@ -26,3 +40,4 @@
 --
 -- SELECT * FROM students
 --     FULL OUTER JOIN notes n on students.id = n.student_id;
+
