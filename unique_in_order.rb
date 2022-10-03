4968b95ee4853aac0c5d417e9251611232e130d5
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-03 19:20:38 +0800

    Added unique_in_order.rb

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -1,7 +1,7 @@
 def unique_in_order(iterable)
   result = []
   iterable = iterable.split("") unless iterable.is_a? Array
-  iterable.each_with_index do |element,index|
+  iterable.each_with_index do | element, index |
     result << element if iterable[index - 1] != element || index == 0
   end
   result
