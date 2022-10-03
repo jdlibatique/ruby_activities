107c873c09736f49ea0384cb98abdd7582e7c79c
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-03 19:27:48 +0800

    Changed condition for if statement

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -2,9 +2,9 @@ def unique_in_order(iterable)
   output = Array.new
   iterable = iterable.split("") unless iterable.is_a? Array
   iterable.each_with_index do | element, index |
-    output << element if iterable[index - 1] != element || index == 0
+    output << element if iterable[index + 1] != element
   end
   output
 end
 
-print unique_in_order('CDABBAS')
+print unique_in_order('AAABBBCCDEE')
