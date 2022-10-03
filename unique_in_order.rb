43629aae72e09f4ed6c52a7b23a5b753136f8d3d
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-03 20:14:10 +0800

    New solution using range object

--- unique_in_order.rb
+++ unique_in_order.rb
@@ -1,10 +1,21 @@
+# def unique_in_order(iterable)
+#   output = Array.new
+#   iterable = iterable.split("") unless iterable.is_a? Array
+#   iterable.each_with_index do | element, index |
+#     output << element if iterable[index + 1] != element
+#   end
+#   output
+# end
+
+# New solution using range object
+
 def unique_in_order(iterable)
   output = Array.new
-  iterable = iterable.split("") unless iterable.is_a? Array
-  iterable.each_with_index do | element, index |
-    output << element if iterable[index + 1] != element
+  (0..iterable.length).each do | index |
+    output << iterable[index] if iterable[index + 1] != iterable[index]
   end
   output
 end
 
 print unique_in_order('AAABBBCCDEE')
+print unique_in_order([1,1,2,3,4,5])
