ccb3b86e9fcc93a97ec38b726103f59a2ce8475b
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-04 19:41:40 +0800

    added test cases

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -6,7 +6,7 @@ def disemvowel(str)
   filter = ['a', 'e', 'i', 'o', 'u']
   output = Array.new
   (0..str.length).each do | index |
-    output << str[index] unless filter.include? str.downcase[index]
+    output << str[index] if filter.none? str.downcase[index]
   end
   output.join("")
 end
