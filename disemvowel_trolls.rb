d0072787389b586a2f6383a526e38c16e1f6581f
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-04 19:15:42 +0800

    disemvowel trolls

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -1,12 +1,4 @@
-# def disemvowel(str)
-#   str.delete("aeiouAEIOU")
-# end
-
 def disemvowel(str)
-  filter = ['a', 'e', 'i', 'o', 'u']
-  output = ""
-  (0..str.length).each do | index |
-    output << str[index] if ['a', 'e', 'i', 'o', 'u'].include? str[index]. != false
-  end
-  output.to_s
+  str.delete("aeiouAEIOU")
 end
+
