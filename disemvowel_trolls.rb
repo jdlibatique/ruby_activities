bd272d448403ae509916051bd687edf7e003c32f
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-10-04 19:30:02 +0800

    changed code

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -1,4 +1,14 @@
+# def disemvowel(str)
+#   str.delete("aeiouAEIOU")
+# end
+
 def disemvowel(str)
-  str.delete("aeiouAEIOU")
+  filter = ['a', 'e', 'i', 'o', 'u']
+  output = Array.new
+  (0..str.length).each do | index |
+    output << str[index] if filter.none? str.downcase[index]
+  end
+  output.join("")
 end
 
+puts disemvowel("this sucks yo")
